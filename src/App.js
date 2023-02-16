4b29303d61bbb61afaf7a38f738d671b06a36ae2
OussamaRguez <oussema.rguez@gmail.com>
2023-02-16 21:05:51 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+    <>
+    <h1>3id thawret Libya, Marhba Zied</h1>
+    <form>
+      <div class="mb-3">
+        <label  for="exampleFormControlInput1" class="form-label lab">Email address</label>
+        <input  type="email" class="form-control inp" id="exampleFormControlInput1" placeholder="name@example.com"/>
+      </div>
+      <div class="mb-3">
+        <label  for="exampleFormControlTextarea1" class="form-label lab">Example textarea</label>
+        <textarea class="form-control inp" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+
+    </form>
+    </>
+
+  )
 }
 
 export default App;
